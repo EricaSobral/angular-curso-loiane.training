c96b8af492ffb994ae5564e89f6b8c60ba751d10
ericasobral <erica.sobral95@gmail.com>
2021-10-15 00:02:18 -0300

    interpolação e Propety Binding

--- 02_data_binding/data-binding/src/app/data-binding/data-binding.component.ts
+++ 02_data_binding/data-binding/src/app/data-binding/data-binding.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
+  url: string = 'www.google.com.br';
+  cursoAngular: boolean = true;
+  urlImagem = 'http://lorempixel.com/400/200/';
+
+  getValor(){
+    return 1;
+  }
+  getCurtirCurso(){
+    return true;
+  }
   constructor() { }
 
   ngOnInit(): void {
