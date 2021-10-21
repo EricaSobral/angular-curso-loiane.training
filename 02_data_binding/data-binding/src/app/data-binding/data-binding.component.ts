b3cb0bf483314ec4ebeae3d0bdb91e84a201f46b
ericasobral <erica.sobral95@gmail.com>
2021-10-21 18:41:19 -0300

    finalizando aula sobre event binding

--- 02_data_binding/data-binding/src/app/data-binding/data-binding.component.ts
+++ 02_data_binding/data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.scss']
+  // styleUrls: ['./data-binding.component.scss']
+  styles: [
+    `
+      .highlight {
+          background-color: yellow;
+          font-weight: bold;
+      }
+    `
+  ]
+
 })
 export class DataBindingComponent implements OnInit {
 
   url: string = 'www.google.com.br';
   cursoAngular: boolean = true;
   urlImagem = 'http://lorempixel.com/400/200/';
+  valorAtual: string = '';
+  valorSalvo = '';
+
+  isMouseOver: boolean = false;
+
 
   getValor(){
     return 1;
@@ -22,4 +36,19 @@ export class DataBindingComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  botaoClicado(){
+    alert
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
 }
